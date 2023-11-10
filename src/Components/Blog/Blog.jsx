a3076009f809039b5413273fbb00f559f851f15f
ShakiraMorium <shakiramorium@gmail.com>
2023-11-10 12:47:36 +0600

    blog component added

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,16 +1,40 @@
 import PropTypes from 'prop-types';
+import { BsFillBookmarksFill } from 'react-icons/fa';
 
 const Blog = ({ blog }) => {
-    console.log(blog)
+    const { title, cover, author, author_img, posted_date, reading_time, hashtags } = blog;
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='w-full  md-8' src={cover} alt={`cover picture of the title ${title}`} />
+
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time}min read</span>
+                    
+                    <button className='ml-2 text-red-600'><BsFillBookmarksFill></BsFillBookmarksFill>
+                    </button>
+                </div>
+            </div>
+            <h2 className='text-4xl mb-4'>{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href={hashtags}></a></span>)
+                }
+            </p>
         </div>
     );
 };
 // props types error solve//
 Blog.propTypes = {
-    blog: PropTypes. object .isRequired
+    blog: PropTypes.object.isRequired
 }
 
 export default Blog;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,8 @@ import Profile from '../../assets/images/profile.png'
 const Header = () => {
 
     return (
-        <div className='flex justify-between items-center p-4 mx-4 border-b-2'>
+        <div className='flex justify-between items-center p-4 max-w-7xl mx-auto'>
+
             <header className='text-4xl font-bold  text-center'>Knowledge Cafe</header>
             <img src={Profile} alt="" />
             
